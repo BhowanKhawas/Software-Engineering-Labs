1f9c33c126a13af56de15cd9ee11cbb4bec7324e
BhowanKhawas <khawasb@roehampton.ac.uk>
2026-02-02 17:55:57 +0000

    Week 2 lab work: JS fundamentals

---
+++ week2/script1.js
@@ -0,0 +1,13 @@
+// Function to change text color to Red
+function turnRed() {
+    // Find the paragraph by its ID
+    var element = document.getElementById("myParagraph");
+    // Change the color style to red
+    element.style['color'] = "red";
+}
+
+// Function to change text color to Blue
+function turnBlue() {
+    var element = document.getElementById("myParagraph");
+    element.style['color'] = "blue";
+}
